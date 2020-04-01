79d37f1df6ca725f11fbaead8f8babb7d1bfb596
Dakota Wicker <dwicker1996@gmail.com>
2020-04-01 10:34:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,10 +10,22 @@ class School
   	roster = {}
   end
 
-  def add_student(name,grade)
-  	roster[grade] = []
-  	roster[grade] << name
-
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
   end
 
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
